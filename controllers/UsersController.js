6308c624b3a72fb38c072b319ba47c1a2702fcd8
OmarElati <5796@holbertonstudents.com>
2024-04-14 14:39:35 +0200

    Create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,26 +1,19 @@
 import sha1 from 'sha1';
 import DBClient from '../utils/db';
-import RedisClient from '../utils/redis';
-
-const { ObjectId } = require('mongodb');
 
 class UsersController {
-  static async createUser(req, res) {
+  static async postNew(req, res) {
     const userEmail = req.body.email;
     if (!userEmail) return res.status(400).json({ error: 'Missing email' });
 
     const userPassword = req.body.password;
     if (!userPassword) return res.status(400).json({ error: 'Missing password' });
 
-    const existingUser = await DBClient.db
-      .collection('users')
-      .findOne({ email: userEmail });
-    if (existingUser) return res.status(400).json({ error: 'Already exists' });
+    const oldUserEmail = await DBClient.db.collection('users').findOne({ email: userEmail });
+    if (oldUserEmail) return res.status(400).json({ error: 'Already exist' });
 
-    const hashedPassword = sha1(userPassword);
-    const result = await DBClient.db
-      .collection('users')
-      .insertOne({ email: userEmail, password: hashedPassword });
+    const shaUserPassword = sha1(userPassword);
+    const result = await DBClient.db.collection('users').insertOne({ email: userEmail, password: shaUserPassword });
 
     return res.status(201).json({ id: result.insertedId, email: userEmail });
   }
