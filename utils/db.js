cbd43a994d59e5f63ea417de3f32c995f0ef4485
OmarElati <5796@holbertonstudents.com>
2024-04-15 00:38:23 +0200

    update

--- utils/db.js
+++ utils/db.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+const {MongoClient} = require('mongodb');
 
 const host = process.env.DB_HOST || 'localhost';
 const port = process.env.DB_PORT || 27017;
@@ -6,32 +6,32 @@ const database = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${host}:${port}`;
 
 class DBClient {
-  constructor() {
-    this.connect();
-  }
+	constructor() {
+		this.connect();
+	}
 
-  async connect() {
-    try {
-      const client = await MongoClient.connect(url, { useUnifiedTopology: true });
-      this.db = client.db(database);
-      console.log('Connected to MongoDB');
-    } catch (error) {
-      console.error('Error connecting to MongoDB:', error);
-      process.exit(1);
-    }
-  }
+	async connect() {
+		try {
+			const client = await MongoClient.connect(url, {useUnifiedTopology: true});
+			this.db = client.db(database);
+			console.log('Connected to MongoDB');
+		} catch (error) {
+			console.error('Error connecting to MongoDB:', error);
+			process.exit(1);
+		}
+	}
 
-  isAlive() {
-    return !!this.db;
-  }
+	isAlive() {
+		return Boolean(this.db);
+	}
 
-  async nbUsers() {
-    return this.db.collection('users').countDocuments();
-  }
+	async nbUsers() {
+		return this.db.collection('users').countDocuments();
+	}
 
-  async nbFiles() {
-    return this.db.collection('files').countDocuments();
-  }
+	async nbFiles() {
+		return this.db.collection('files').countDocuments();
+	}
 }
 
 const dbClient = new DBClient();
--- utils/redis.js
+++ utils/redis.js
@@ -1,34 +1,34 @@
 const redis = require('redis');
-const { promisify } = require('util');
+const {promisify} = require('util');
 
 class RedisClient {
-  constructor() {
-    this.client = redis.createClient();
+	constructor() {
+		this.client = redis.createClient();
 
-    this.getAsync = promisify(this.client.get).bind(this.client);
+		this.getAsync = promisify(this.client.get).bind(this.client);
 
-    this.client.on('error', (error) => {
-      console.error(`Redis client not connected to the server: ${error}`);
-    });
-  }
+		this.client.on('error', error => {
+			console.error(`Redis client not connected to the server: ${error}`);
+		});
+	}
 
-  isAlive() {
-    return this.client.connected;
-  }
+	isAlive() {
+		return this.client.connected;
+	}
 
-  async get(key) {
-    const value = await this.getAsync(key);
-    return value;
-  }
+	async get(key) {
+		const value = await this.getAsync(key);
+		return value;
+	}
 
-  async set(key, value, duration) {
-    this.client.set(key, value);
-    this.client.expire(key, duration);
-  }
+	async set(key, value, duration) {
+		this.client.set(key, value);
+		this.client.expire(key, duration);
+	}
 
-  async del(key) {
-    this.client.del(key);
-  }
+	async del(key) {
+		this.client.del(key);
+	}
 }
 
 const redisClient = new RedisClient();
