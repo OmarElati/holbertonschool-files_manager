e998780f8aabbae9df350c92639ba6ad1d2e26b3
OmarElati <5796@holbertonstudents.com>
2024-04-14 15:45:44 +0200

    Updated

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
-import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController'; 
+
+const express = require('express');
 
 const router = (app) => {
   const route = express.Router();
   app.use(express.json());
   app.use('/', route);
 
-  route.get('/status', (req, res) => AppController.getStatus(req, res));
-  route.get('/stats', (req, res) => AppController.getStats(req, res));
-  route.post('/users', (req, res) => UsersController.postNew(req, res));
+  route.get('/status', (request, response) => AppController.getStatus(request, response));
+  route.get('/stats', (request, response) => AppController.getStats(request, response));
+  route.post('/users', (request, response) => UsersController.postNew(request, response));
   route.get('/connect', (request, response) => AuthController.getConnect(request, response));
   route.get('/disconnect', (request, response) => AuthController.getDisconnect(request, response));
   route.get('/users/me', (request, response) => UsersController.getMe(request, response));
